672cc39fb8d7f03b1adc7d8613dad660dc196dd8
APTXA <2863565743@qq.com>
2023-03-08 23:02:05 +0800

    2023-3-8

--- prac/IO.c
+++ prac/IO.c
@@ -2,30 +2,25 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/tcp.h>
 
 int main(int argv,char* argc[])
 {
     int sock,state;
-    int res;
-    socklen_t len=sizeof(res);
+    int len=sizeof(state);
 
     sock=socket(PF_INET,SOCK_STREAM,0);
-    //len=sizeof(res);
-    if(state=getsockopt(sock,SOL_SOCKET,SO_SNDBUF,(void*)&res,&len)==-1){
-        printf("getsockopt failed \n");
-    }
-    else{
-        printf("snd_buf size is %d \n",res);
-    }
 
-    //len=sizeof(res);
-    if(state=getsockopt(sock,SOL_SOCKET,SO_RCVBUF,(void*)&res,&len)==-1){
-        printf("getsockopt failed\n");
-    }
-    else{
-        printf("rcv_buf size is %d \n",res);
-    }
-    
+    getsockopt(sock,IPPROTO_TCP,TCP_NODELAY,(void*)&state,&len);
+
+    printf("default Nagle algo is %d\n",state);
+
+    state=1;
+
+    setsockopt(sock,IPPROTO_TCP,TCP_NODELAY,(void*)&state,len);
+
+    printf("after set Nagle algo is %d \n",state);
 
     
     
